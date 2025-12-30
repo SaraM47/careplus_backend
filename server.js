89570245f06135dea1d9b534bd32c3b9565f1066
SaraMsoso <Sara.soder44@gmail.com>
2025-12-30 11:49:52 +0100

    Implemented JWT auth, RBAC middleware, CORS, multipart support and route registration role-based access control in Fastify server

---
+++ server.js
@@ -0,0 +1,82 @@
+const Fastify = require("fastify");
+const cors = require("@fastify/cors");
+const jwt = require("@fastify/jwt");
+const multipart = require("@fastify/multipart");
+
+const productRoutes = require("./routes/product.route");
+const categoryRoutes = require("./routes/category.route");
+const authRoutes = require("./routes/auth.route");
+
+const fastify = Fastify({ logger: true });
+
+/**
+ * CORS
+ * Allows requests from the frontend (Vue SPA)
+ */
+fastify.register(cors, {
+  origin: true,
+});
+
+/**
+ * JWT plugin used for authentication, register and login
+ */
+fastify.register(jwt, {
+  secret: process.env.JWT_SECRET || "dev_secret_change_me",
+});
+
+/**
+ * Multipart is used for file image uploads (product images)
+ */
+fastify.register(multipart, {
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
+});
+
+/**
+ * Auth hook for protected routes which requires a valid JWT token
+ */
+fastify.decorate("auth", async (req, reply) => {
+    try {
+      const decoded = await req.jwtVerify();
+      req.user = decoded; 
+    } catch (err) {
+      reply.code(401).send({ message: "Unauthorized" });
+    }
+  });
+  
+  /**
+   * Role-based access control is used to protect routes based on user roles, which requires authentication to already be executed
+   */
+  fastify.decorate("requireRole", (roles) => {
+    return async (req, reply) => {
+      if (!req.user) {
+        return reply.code(401).send({ message: "Unauthorized" });
+      }
+  
+      if (!roles.includes(req.user.role)) {
+        return reply.code(403).send({ message: "Forbidden" });
+      }
+    };
+  });  
+
+/**
+ * Routes
+ * /auth for authentication login and registration (public)
+ * /categories for cateqgory management (protected)
+ * /products for product management (protected)
+ */
+fastify.register(authRoutes, { prefix: "/auth" });
+fastify.register(categoryRoutes, { prefix: "/categories" });
+fastify.register(productRoutes, { prefix: "/products" });
+
+/**
+ * Start server
+ */
+fastify.listen({ port: 5000 }, (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log("Backend running on http://localhost:5000");
+});
